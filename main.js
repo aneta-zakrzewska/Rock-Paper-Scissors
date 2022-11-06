3f8a041f54da10a733268df4c5d3b4b02cc1e522
aneta-zakrzewska <100867524+aneta-zakrzewska@users.noreply.github.com>
2022-11-06 22:04:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,71 @@
-const resultDisplay = document.querySelector('#result')
-const choicesDisplay = document.querySelector('#choices')
-const choices = ['rock', 'paper', 'scissors']
+const choice = document.querySelector("choice");
+const button = document.querySelector("button");
 
-const handleClick = (e) => {
-  getResults(e.target.innerHTML, choices[Math.floor(Math.random() * choices.length)])
-}
+const paperChosen = document.querySelector("#paper");
+const rockChosen = document.querySelector("#rock");
+const scissorsChosen = document.querySelector("#scissors");
+
+const resultDisplay = document.querySelector(".result");
+const choicesDisplay = document.querySelector(".choices");
+
+let userChoice = [paperChosen, rockChosen, scissorsChosen];
+
+paperChosen.addEventListener("click", e => console.log("paper"));
+rockChosen.addEventListener("click", e => console.log("rock"));
+scissorsChosen.addEventListener("click", e => console.log("scissors")); //moj wybor w konsoli
+
+//make it after click:
+
+let choices = ["rock", "paper", "scissors"];
+
+let mickeyChoice = choices => {
+	getResults(
+		e.target.choices,
+		choices[Math.floor(Math.random() * choices.length)]
+	); //wybór myszki miki w konsoli
+	console.log(mickeyChoice);
+};
+
 choices.forEach(choice => {
-  const button = document.createElement('button')
-  button.innerHTML = choice
-  button.addEventListener('click', handleClick)
-  choicesDisplay.appendChild(button)
-})
+	const button = document.createElement("button");
+	button.innerHTML = choice;
+	button.addEventListener("click", choicesDisplay);
+	choicesDisplay.appendChild(button);
+});
 
-const getResults = (userChoice, computerChoice) => {
-  switch (userChoice + computerChoice) {
-    case 'scissorspaper':
-    case 'rockscissors':
-    case 'paperrock':
-      resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU WIN!'
-      break
-    case 'paperscissors':
-    case 'scissorsrock':
-    case 'rockpaper':
-      resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU LOSE!'
-      break
-    case 'scissorsscissors':
-    case 'rockrock':
-    case 'paperpaper':
-      resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , ITS A DRAW!'
-      break
-  }
-}
+//clicking on users choice causes running of mickeys choice
+
+const getResults = (userChoice, mickeyChoice) => {
+	if (
+		(userChoice === 'rock' && mickeyChoice === 'scissors') ||
+		(userChoice === 'scissors' && mickeyChoice === 'paper') ||
+		(userChoice === 'paper' && mickeyChoice === 'rock')
+	) {
+		resultDisplay.innerHTML =
+			'You chose ' +
+			userChoice +
+			' and Mickey chose ' +
+			mickeyChoice +
+			' , YOU WIN!';
+	} else if (
+		(userChoice === 'scissors' && mickeyChoice === 'rock') ||
+		(userChoice === 'paper' && mickeyChoice === 'scissors') ||
+		(userChoice === 'rock' && mickeyChoice === 'paper')
+	) {
+		resultDisplay.innerHTML =
+			'You chose ' +
+			userChoice +
+			' and Mickey chose ' +
+			mickeyChoice +
+			' , YOU LOSE!';
+	} else {
+		resultDisplay.innerHTML =
+			'You chose ' +
+			userChoice +
+			' and Mickey chose ' +
+			mickeyChoice +
+			' , ITS A DRAW!';
+	}
+};
+
+button.addEventListener('click', getResults);
